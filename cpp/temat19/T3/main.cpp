3ff7578e87d249ede42cf21facf5d281707582b0
DJMixu <lolniak13@interia.pl>
2020-11-02 12:08:51 +0100

    replace

--- cpp/temat19/T3/main.cpp
+++ cpp/temat19/T3/main.cpp
@@ -4,51 +4,42 @@ using namespace std;
 //Autor Michal Majczek
 int main()
 {
-    int n;
-    cout<<"Podaj ile elementow w tablicy: ";
-    cin>>n;
-    int tab[n];
-    for(int i=0; i<n; i++)
-    {
+        int n;
+        cout<<"Podaj ile elementow w tablicy: ";
+        cin>>n;
+        int tab[n];
+        for(int i=0; i<n; i++)
+        {
         cout<<"tab["<<i<<"]=";
         cin>>tab[i];
-
-    }
-
+        }
         int max;
         max=tab[0];
         for(int i=0; i<n; i++)
         {
             if(max<tab[i])
             max=tab[i];
-
         }
         cout<<"Najwieksza wartosc tablicy: "<<max;
-
         cout<<"\nineksy wart. max: ";
         for(int i=0; i<n; i++)
         {
            if(max==tab[i])cout<<i<<", ";
-
         }
-            cout<<endl<<endl;
+         cout<<endl<<endl;
         int min;
         min=tab[0];
         for(int i=0; i<n; i++)
         {
             if(min>tab[i])
             min=tab[i];
-
         }
         cout<<"Najmniejsza wartosc tablicy: "<<min;
-
         cout<<"\nineksy wart. min: ";
         for(int i=0; i<n; i++)
         {
            if(min==tab[i])cout<<i<<", ";
-
         }
-
-
+		cout<<"\nKoniec programu";
     return 0;
 }
