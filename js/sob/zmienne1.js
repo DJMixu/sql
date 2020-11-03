bc0ef5f08078247098daf09321a7fe5ec2852755
DJMixu <lolniak13@interia.pl>
2020-11-03 14:14:35 +0100

    03.11.2020 14.14

---
+++ js/sob/zmienne1.js
@@ -0,0 +1,14 @@
+var a = 6;
+var b = 055;
+var c = 0x2A;
+var d = 0.88E-3;
+var e = 'Chcę się nauczyć JavaScript, chcę się nauczyć JavaScrpit';
+var f = null;
+var g;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(g);
